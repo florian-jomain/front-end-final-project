5efa8fa291247487c119cc6d1bb8e05cf2cb4a32
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-18 17:22:20 +0200

    feat: add dropdown menu

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -8,6 +8,7 @@ import Tippy from '../components/UI/Tippy'
 import '../App.scss'
 
 const NavMain = (props) => {
+  const [isHoverd, setIsHovered] = React.useState(false)
   const { context } = props
 
   function handleLogout() {
@@ -21,15 +22,48 @@ const NavMain = (props) => {
       })
   }
 
-  function DropdownContent({ hide }) {
+  function DropdownContent() {
     return (
       <React.Fragment>
-        <div className="dropdown">
-          <a href="/user-profile">Profile page</a>
-          <p onClick={handleLogout}>Logout</p>
+        <div
+          className="dropdown"
+          onMouseEnter={handleMouseHover}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className="user__menu">
+            <div className="user__image round__image navbar__image">
+              <img src={context.user.image} alt="" />
+            </div>
+            <div>
+              <span>{context.user.name}</span>
+              <span>@{context.user.username}</span>
+            </div>
+          </div>
+          <ul>
+            <li>
+              <span>
+                <a href="/user-profile">Profile page</a>
+              </span>
+            </li>
+            <li>
+              <span onClick={handleLogout}>Logout</span>
+            </li>
+          </ul>
         </div>
       </React.Fragment>
     )
+  }
+
+  function handleMouseHover() {
+    setTimeout(() => {
+      setIsHovered(true)
+    }, 0)
+  }
+
+  function handleMouseLeave() {
+    setTimeout(() => {
+      setIsHovered(false)
+    }, 4000)
   }
 
   return (
@@ -57,14 +91,20 @@ const NavMain = (props) => {
                 interactive={true}
                 delay={(null, 200)}
                 hideOnClick={false}
+                // trigger="click"
               >
-                <div className="user__menu">
+                <div
+                  className="user__menu"
+                  onMouseEnter={handleMouseHover}
+                  onMouseLeave={handleMouseLeave}
+                >
                   <div className="user__image round__image navbar__image">
                     <img src={context.user.image} alt="" />
                   </div>
 
-                  {context.user && context.user.name}
-                  <div className="chevron">
+                  {/* {context.user && context.user.name} */}
+
+                  <div className={`chevron ${isHoverd ? 'rotate' : ''}`}>
                     <img src="../../media/chevron.svg" alt="" />
                   </div>
                 </div>
--- src/components/UI/Tippy.jsx
+++ src/components/UI/Tippy.jsx
@@ -1,11 +1,4 @@
-import React from 'react'
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css' // optional
-
-const JSXContent = () => (
-  <Tippy content={<span>Tooltip</span>}>
-    <button>My button</button>
-  </Tippy>
-)
 
 export default Tippy
