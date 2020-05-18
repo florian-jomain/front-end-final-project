92b78f3385e58571c487374fd2247877c9e1e8fb
Florian Jomain <florian.jomain@gmail.com>
2020-05-18 16:28:16 +0200

    feat: projects page + projects cards

--- src/components/UI/ProjectCard.jsx
+++ src/components/UI/ProjectCard.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
 function ProjectCard(props) {
+  const { image, title, description, id_tags } = props.project;
   console.log(props.project);
-  const { image, title, description } = props.project;
   return (
-    <div>
-      <div className='ProjectCard'></div>
-      <img className='ProjectCard__image' src={image} alt={title} />
-      <div className='ProjectCard__text'>
-        <h3 className='ProjectCard__title'>{title}</h3>
-        <p>{description}</p>
+    <React.Fragment>
+      <div className='ProjectCard'>
+        <img className='ProjectCard__image' src={image} alt={title} />
+        <div className='ProjectCard__text'>
+          <h3>{title}</h3>
+          <div className='ProjectCard__charity'>
+            <h5>{title}</h5>
+          </div>
+          <div className='ProjectCard__tags'>
+            {id_tags.map((tag) => (
+              <p className='ProjectCard__tag'>{tag.label}</p>
+            ))}
+          </div>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import ProjectCard from "../components/UI/ProjectCard";
+import Filter from "../components/UI/Filter";
 import axios from "axios";
 import { Link } from "react-router-dom";
+// import TextContainer from "../components/UI/TextContainer";
 
 export class Projects extends Component {
   state = {
@@ -12,7 +14,6 @@ export class Projects extends Component {
     axios
       .get("http://localhost:4000/api/projects")
       .then((apiResponse) => {
-        console.log(apiResponse);
         this.setState({ projects: apiResponse.data });
       })
       .catch((apiError) => {
@@ -22,20 +23,24 @@ export class Projects extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Test</h1>
-        <div className='filter'></div>
-        {this.state.projects.map((project, index) => (
-          <Link
-            exact
-            className='links'
-            key={index}
-            to={`/projects/${this.state.projects[index]._id}`}
-          >
-            <ProjectCard key={index} index={index} project={project} />
-          </Link>
-        ))}
-      </div>
+      <React.Fragment>
+        <div className='Projects'>
+          {/* <TextContainer /> */}
+          <Filter className='Projects__filter' />
+          <div className='Projects__cards'>
+            {this.state.projects.map((project, index) => (
+              <Link
+                exact
+                className='Projects__links'
+                key={index}
+                to={`/projects/${this.state.projects[index]._id}`}
+              >
+                <ProjectCard key={index} index={index} project={project} />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
