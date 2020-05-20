f19041028012e8221fec1a40ba9963c27165dfb8
Florian Jomain <florian.jomain@gmail.com>
2020-05-20 16:45:50 +0200

    feat + style: navbar styling + charity avatar

--- src/components/Forms/FormSignin.jsx
+++ src/components/Forms/FormSignin.jsx
@@ -1,57 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import UserContext from '../Auth/UserContext'
-import { withRouter } from 'react-router-dom'
-import apiHandler from '../../api/apiHandler'
-import Button from '../../components/UI/Button'
-import ErrorMessageHandler from './ErrorMessageHandler'
+import UserContext from "../Auth/UserContext";
+import { withRouter } from "react-router-dom";
+import apiHandler from "../../api/apiHandler";
+import Button from "../../components/UI/Button";
+import ErrorMessageHandler from "./ErrorMessageHandler";
 
 class FormSignin extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   state = {
-    email: '',
-    password: '',
-    userType: '',
-  }
+    email: "",
+    password: "",
+    userType: "",
+  };
 
   handleChange = (event) => {
-    const key = event.target.name
-    this.setState({ [key]: event.target.value })
-  }
+    const key = event.target.name;
+    this.setState({ [key]: event.target.value });
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    let errors = { is: false, messages: [] }
+    let errors = { is: false, messages: [] };
 
     if (!this.state.password) {
-      errors.is = true
-      errors.messages.push('Enter your password')
+      errors.is = true;
+      errors.messages.push("Enter your password");
     }
 
     if (!this.state.email) {
-      errors.is = true
-      errors.messages.push('Enter your email')
+      errors.is = true;
+      errors.messages.push("Enter your email");
     }
 
     if (errors.is) {
-      this.setState({ errors: errors.messages })
+      this.setState({ errors: errors.messages });
     } else {
       apiHandler
         .signin(this.state)
         .then((data) => {
-          console.log(data)
-          this.context.setUser(data)
-          this.props.history.push('/')
+          this.context.setUser(data);
+          this.props.history.push("/");
         })
         .catch((error) => {
-          errors.is = true
-          errors.messages.push(error.response.data.message)
-          this.setState({ errors: errors.messages })
-        })
+          errors.is = true;
+          errors.messages.push(error.response.data.message);
+          this.setState({ errors: errors.messages });
+        });
     }
-  }
+  };
 
   render() {
     return (
@@ -60,20 +59,20 @@ class FormSignin extends Component {
           <ErrorMessageHandler messages={this.state.errors} />
         )}
         <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-          <div className="form__group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
+          <div className='form__group'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' name='email' />
           </div>
 
-          <div className="form__group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" />
+          <div className='form__group'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' name='password' />
           </div>
-          <Button type="primary">Let's go!</Button>
+          <Button type='primary'>Let's go!</Button>
         </form>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default withRouter(FormSignin)
+export default withRouter(FormSignin);
