2a8bed3028f780cae2f171676c685b17e0d86a08
Florian Jomain <florian.jomain@gmail.com>
2020-05-21 18:46:54 +0200

    feat: responsiveness + styling + empty tags not displaying

--- src/components/UI/HelperCard.jsx
+++ src/components/UI/HelperCard.jsx
@@ -3,6 +3,7 @@ import Tag from "./Tag";
 
 function HelperCard(props) {
   const { image, username, skills, bio } = props.helper;
+  console.log(props);
 
   return (
     <React.Fragment>
@@ -11,11 +12,13 @@ function HelperCard(props) {
         <div className='HelperCard__text'>
           <h3>{username}</h3>
           <div className='HelperCard__tags'>
-            {skills.map((tag, index) => (
-              <Tag className='HelperCard__tag' key={index} tag={tag} />
-            ))}
+            {skills.length > 0 &&
+              skills[0] !== "" &&
+              skills.map((tag, index) => (
+                <Tag className='HelperCard__tag' key={index} tag={tag} />
+              ))}
           </div>
-          <p className='HelperCard__description'>{bio}</p>
+          <p className='HelperCard__bio'>{bio}</p>
         </div>
       </div>
     </React.Fragment>
--- src/components/UI/Projects.jsx
+++ src/components/UI/Projects.jsx
@@ -1,69 +1,68 @@
-import React from 'react'
-import { withUser } from '../Auth/withUser'
-import Button from './Button'
-import Tag from './Tag'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { withUser } from "../Auth/withUser";
+import Button from "./Button";
+import Tag from "./Tag";
+import { NavLink } from "react-router-dom";
 
 function Projects(props) {
-  const { context } = props
-  console.log(props.projects)
+  const { context } = props;
 
-  if (!props.projects) return null
+  if (!props.projects) return null;
   if (
     props.projects.id_projects.length === 0 &&
     context.user &&
-    context.user.userType === 'helper'
+    context.user.userType === "helper"
   )
     return (
-      <div className="emptyStateProjects">
-        <img src="../../media/teammates-5.svg" alt="empty state" />
+      <div className='emptyStateProjects'>
+        <img src='../../media/teammates-5.svg' alt='empty state' />
         <p>You have no projects</p>
-        <NavLink to={'/projects'}>
-          <Button type="tertiary">Browse projects to join</Button>
+        <NavLink to={"/projects"}>
+          <Button type='tertiary'>Browse projects to join</Button>
           <p></p>
         </NavLink>
       </div>
-    )
+    );
   if (
     props.projects.id_projects.length === 0 &&
     context.user &&
-    context.user.userType === 'charity'
+    context.user.userType === "charity"
   )
     return (
-      <div className="emptyStateProjects">
-        <img src="../../media/teammates-5.svg" alt="empty state" />
+      <div className='emptyStateProjects'>
+        <img src='../../media/teammates-5.svg' alt='empty state' />
         <p>You have no projects</p>
-        <NavLink to={'/create-project'}>
-          <Button type="tertiary">Create a project</Button>
+        <NavLink to={"/create-project"}>
+          <Button type='tertiary'>Create a project</Button>
         </NavLink>
       </div>
-    )
+    );
 
   if (props.projects.id_projects.length === 0)
     return (
-      <div className="emptyStateProjects">
-        <img src="../../media/teammates-5.svg" alt="empty state" />
+      <div className='emptyStateProjects'>
+        <img src='../../media/teammates-5.svg' alt='empty state' />
         <p>You have no projects</p>
       </div>
-    )
+    );
   else {
     return (
       <React.Fragment>
-        <div className="project">
+        <div className='project'>
           {props.projects.id_projects.map((project) => (
-            <NavLink to={'/projects/' + project._id}>
-              <div className="ProjectCard" key={project.title}>
+            <NavLink to={"/projects/" + project._id}>
+              <div className='ProjectCard' key={project.title}>
                 <img
-                  className="ProjectCard__image"
+                  className='ProjectCard__image'
                   src={project.image}
                   alt={project.title}
                 />
-                <div className="ProjectCard__text">
+                <div className='ProjectCard__text'>
                   <h3>{project.title}</h3>
                   <p>{project.category}</p>
                 </div>
 
-                <div className="ProjectCard__tags">
+                <div className='ProjectCard__tags'>
                   {project.skills.map((tag, index) => (
                     <Tag key={index} tag={tag} />
                   ))}
@@ -74,8 +73,8 @@ function Projects(props) {
           ))}
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default withUser(Projects)
+export default withUser(Projects);
