6788c8c11215eea16085434782d3d3ce15703a42
Florian Jomain <florian.jomain@gmail.com>
2020-05-21 08:35:47 +0200

    style: close button on pop up + some styling

--- src/components/UI/Articles.jsx
+++ src/components/UI/Articles.jsx
@@ -4,8 +4,12 @@ export default function Articles() {
   return (
     <div className='articles__container'>
       <div>
-        <img className='articleImg' src='../../media/avatar.png' alt='icon' />
-        <h3>Friendly</h3>
+        <img
+          className='articles__image'
+          src='../../media/avatar.png'
+          alt='icon'
+        />
+        <h3 className='articles__header serif'>Friendly</h3>
         <p>
           You may not know much about the ins and outs of creating a website but
           don't worry, if you don't our members do and can guide you through the
@@ -13,8 +17,12 @@ export default function Articles() {
         </p>
       </div>
       <div>
-        <img className='articleImg' src='../../media/avatar.png' alt='icon' />
-        <h3>Fast</h3>
+        <img
+          className='articles__image'
+          src='../../media/avatar.png'
+          alt='icon'
+        />
+        <h3 className='articles__header serif'>Fast</h3>
         <p>
           You may be hoping to have your website ready in a short amount of
           time, that is why we have created a "team" feature, where several
@@ -22,8 +30,12 @@ export default function Articles() {
         </p>
       </div>
       <div>
-        <img className='articleImg' src='../../media/avatar.png' alt='icon' />
-        <h3>Free</h3>
+        <img
+          className='articles__image'
+          src='../../media/avatar.png'
+          alt='icon'
+        />
+        <h3 className='articles__header serif'>Free</h3>
         <p>
           Our members care just as much as you do, and no remuneration will be
           expected. Everyone here just wants to help as best as they can with
