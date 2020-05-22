e8d9e1bd39b735a4f4f949ccb764307a4afb079e
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-22 11:54:37 +0200

    fix styling

--- src/components/UI/HelperCard.jsx
+++ src/components/UI/HelperCard.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-import Tag from "./Tag";
+import React from 'react'
+import Tag from './Tag'
 
 function HelperCard(props) {
-  const { image, username, skills, bio } = props.helper;
+  const { image, username, skills, bio } = props.helper
 
   return (
     <React.Fragment>
-      <div className='HelperCard'>
-        <img className='HelperCard__image' src={image} alt={username} />
-        <div className='HelperCard__text'>
+      <div className="HelperCard">
+        <img className="HelperCard__image" src={image} alt={username} />
+        <div className="HelperCard__text">
           <h3>{username}</h3>
-          <div className='HelperCard__tags'>
+          <div className="HelperCard__tags">
             {skills.length > 0 &&
-              skills[0] !== "" &&
+              skills[0] !== '' &&
               skills.map((tag, index) => (
-                <Tag className='HelperCard__tag' key={index} tag={tag} />
+                <Tag className="HelperCard__tag" key={index} tag={tag} />
               ))}
           </div>
-          <p className='HelperCard__bio'>{bio}</p>
+          <div className="HelperCard__bio">
+            <p>{bio}</p>
+          </div>
         </div>
       </div>
     </React.Fragment>
-  );
+  )
 }
 
-export default HelperCard;
+export default HelperCard
