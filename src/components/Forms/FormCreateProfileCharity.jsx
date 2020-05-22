25e20b8130a50f75b377ff98a558e19b9cb551ec
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-22 10:06:47 +0200

    fix: image showing on create project

--- src/components/Forms/FormCreateProfileCharity.jsx
+++ src/components/Forms/FormCreateProfileCharity.jsx
@@ -1,125 +1,125 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import UserContext from "../Auth/UserContext";
-import { withUser } from "../../components/Auth/withUser";
-import apiHandler from "../../api/apiHandler";
-import Button from "../../components/UI/Button";
-import LocationAutoComplete from "../LocationAutoComplete";
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import UserContext from '../Auth/UserContext'
+import { withUser } from '../../components/Auth/withUser'
+import apiHandler from '../../api/apiHandler'
+import Button from '../../components/UI/Button'
+import LocationAutoComplete from '../LocationAutoComplete'
 
 class FormCreateProfileCharity extends Component {
-  static contextType = UserContext;
+  static contextType = UserContext
 
   state = {
-    name: "",
+    name: '',
     links: [],
-    bio: "",
-    location: "",
-    tempUrl: "",
-  };
+    bio: '',
+    location: '',
+    tempUrl: '',
+  }
 
   handleChange = (event) => {
-    let key = event.target.name;
+    let key = event.target.name
     switch (event.target.type) {
-      case "file":
+      case 'file':
         this.setState({
           tempUrl: URL.createObjectURL(event.target.files[0]),
           image: event.target.files[0],
-        });
-        break;
-      case "checkbox":
-        this.setState({ [key]: event.target.checked });
-        break;
+        })
+        break
+      case 'checkbox':
+        this.setState({ [key]: event.target.checked })
+        break
       default:
-        this.setState({ [key]: event.target.value });
+        this.setState({ [key]: event.target.value })
     }
-  };
+  }
 
   handlePlace = (place) => {
-    this.setState({ location: place.place_name });
-  };
+    this.setState({ location: place.place_name })
+  }
 
   handleSubmit = (event) => {
-    event.preventDefault();
-    var formData = new FormData();
+    event.preventDefault()
+    var formData = new FormData()
 
     if (this.state.name) {
-      formData.append("name", this.state.name);
+      formData.append('name', this.state.name)
     }
 
     if (this.state.image) {
-      formData.append("image", this.state.image);
+      formData.append('image', this.state.image)
     }
 
     if (this.state.links) {
-      formData.append("links", this.state.links);
+      formData.append('links', this.state.links)
     }
 
     if (this.state.bio) {
-      formData.append("bio", this.state.bio);
+      formData.append('bio', this.state.bio)
     }
 
     if (this.state.location) {
-      formData.append("location", this.state.location);
+      formData.append('location', this.state.location)
     }
 
     apiHandler
       .createProfileCharity(formData)
       .then((data) => {
-        this.context.setUser(data);
-        this.props.history.push("/user-profile");
+        this.context.setUser(data)
+        this.props.history.push('/user-profile')
       })
       .catch((error) => {
-        console.log(error);
-      });
-  };
+        console.log(error)
+      })
+  }
 
   render() {
-    let imgSrc = null;
+    let imgSrc = null
     if (!this.context.user) {
-      this.props.history.push("/");
-      return true;
-    } else if (this.state.tempUrl === "") {
-      imgSrc = this.context.user.image;
-    } else imgSrc = this.state.tempUrl;
+      this.props.history.push('/')
+      return true
+    } else if (this.state.tempUrl === '') {
+      imgSrc = this.context.user.image
+    } else imgSrc = this.state.tempUrl
     return (
       <React.Fragment>
-        <div className='input__form'>
-          <div className='user__image round__image'>
+        <div className="input__form">
+          <div className="user__image round__image">
             <img src={imgSrc} alt={this.context.user.username} />
           </div>
         </div>
 
         <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-          <div className='form__group'>
-            <label htmlFor='image' className='custom-upload label'>
+          <div className="form__group">
+            <label htmlFor="image" className="custom-upload label">
               Upload profile image
             </label>
-            <input className='input' type='file' id='image' name='image' />
+            <input className="input" type="file" id="image" name="image" />
           </div>
 
-          <div className='form__group'>
-            <label htmlFor='name'>Name</label>
+          <div className="form__group">
+            <label htmlFor="name">Name</label>
             <input
-              type='text'
-              id='name'
-              name='name'
-              placeholder='Enter the name of the organization'
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter the name of the organization"
               defaultValue={this.context.user.name}
             />
           </div>
 
-          <div className='form__group'>
-            <label htmlFor='bio'>Your Bio</label>
+          <div className="form__group">
+            <label htmlFor="bio">Your Bio</label>
             <textarea
-              type='textarea'
-              id='bio'
-              name='bio'
-              placeholder='Write something about your organization'
+              type="textarea"
+              id="bio"
+              name="bio"
+              placeholder="Write something about your organization"
               defaultValue={this.context.user.bio}
             />
           </div>
-          <div className='form__group'>
-            <label htmlFor='location'>Location</label>
+          <div className="form__group">
+            <label htmlFor="location">Location</label>
             <LocationAutoComplete
               onSelect={this.handlePlace}
               defaultValue={this.context.user.location}
@@ -133,21 +133,21 @@ class FormCreateProfileCharity extends Component {
             /> */}
           </div>
 
-          <div className='form__group'>
-            <label htmlFor='links'>Links</label>
+          <div className="form__group">
+            <label htmlFor="links">Links</label>
             <input
-              type='text'
-              id='links'
-              name='links'
-              placeholder='For example your website'
+              type="text"
+              id="links"
+              name="links"
+              placeholder="For example your website"
               defaultValue={this.context.user.links}
             />
           </div>
-          <Button type='primary'>Update your profile</Button>
+          <Button type="primary">Update your profile</Button>
         </form>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default withUser(withRouter(FormCreateProfileCharity));
+export default withUser(withRouter(FormCreateProfileCharity))
--- src/components/Forms/FormCreateProject.jsx
+++ src/components/Forms/FormCreateProject.jsx
@@ -1,292 +1,290 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import UserContext from "../Auth/UserContext";
-import apiHandler from "../../api/apiHandler";
-import Button from "../../components/UI/Button";
-import { TagBox } from "react-tag-box";
-import ErrorMessageHandler from "./ErrorMessageHandler";
-import LocationAutoComplete from "../LocationAutoComplete";
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import UserContext from '../Auth/UserContext'
+import apiHandler from '../../api/apiHandler'
+import Button from '../../components/UI/Button'
+import { TagBox } from 'react-tag-box'
+import ErrorMessageHandler from './ErrorMessageHandler'
+import LocationAutoComplete from '../LocationAutoComplete'
 
 const options = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Node.js",
-  "Express",
-  "MongoDB",
-  "Ruby",
-  "Rails",
-  "MySQL",
-  "NoSQL",
-  "PHP",
-  "Python",
-  "C",
-  "C++",
-  "TypeScript",
-  "Vue.js",
-  "Angular",
-  "Java",
-  "Docker",
-  "Azure",
-  "React Native",
-  "Swift",
-  "Redux",
-  "GraphQL",
-  "Android",
-  "Fullstack",
-  "Bootstrap",
-  "Spring",
-  "PostgreSQL",
-  "Agile methodology",
-  "jhipster",
-  "Heroku",
-  "Netlify",
-  "Wordpress",
-  "Ionic",
-  "Firebase",
-  "Meteor.js",
-  "API",
-  "Django",
-  "Git",
-  "Symfony",
-  "Scrum",
-  "Laravel",
-  "Semantic UI",
-  "jQuery",
-  "Figma",
-  "Sketch",
-  "InVision",
-  "Zeplin",
-  "Adobe Photoshop",
-  "Adobe InDesign",
-  "Adobe Illustrator",
-  "Product Design",
-  "User Testing",
-  "UI Design",
-  "UX Design",
-  "Design Thinking",
-  "Service Design",
-  "Logo Design",
-  "Graphic Design",
-  "Print Design",
-  "Workshops",
-  "Wireframing",
-  "Webdesign",
-  "Artistic Direction",
-  "Mobile Design",
-  "Project Management",
-  "Reporting",
-  "Data analytics",
-  "Web analytics",
-  "Product Management",
-  "Design Sprint",
-  "E-commerce",
-  "Go",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Node.js',
+  'Express',
+  'MongoDB',
+  'Ruby',
+  'Rails',
+  'MySQL',
+  'NoSQL',
+  'PHP',
+  'Python',
+  'C',
+  'C++',
+  'TypeScript',
+  'Vue.js',
+  'Angular',
+  'Java',
+  'Docker',
+  'Azure',
+  'React Native',
+  'Swift',
+  'Redux',
+  'GraphQL',
+  'Android',
+  'Fullstack',
+  'Bootstrap',
+  'Spring',
+  'PostgreSQL',
+  'Agile methodology',
+  'jhipster',
+  'Heroku',
+  'Netlify',
+  'Wordpress',
+  'Ionic',
+  'Firebase',
+  'Meteor.js',
+  'API',
+  'Django',
+  'Git',
+  'Symfony',
+  'Scrum',
+  'Laravel',
+  'Semantic UI',
+  'jQuery',
+  'Figma',
+  'Sketch',
+  'InVision',
+  'Zeplin',
+  'Adobe Photoshop',
+  'Adobe InDesign',
+  'Adobe Illustrator',
+  'Product Design',
+  'User Testing',
+  'UI Design',
+  'UX Design',
+  'Design Thinking',
+  'Service Design',
+  'Logo Design',
+  'Graphic Design',
+  'Print Design',
+  'Workshops',
+  'Wireframing',
+  'Webdesign',
+  'Artistic Direction',
+  'Mobile Design',
+  'Project Management',
+  'Reporting',
+  'Data analytics',
+  'Web analytics',
+  'Product Management',
+  'Design Sprint',
+  'E-commerce',
+  'Go',
 ].map((option) => ({
   label: option.toLowerCase(),
   value: option.toLowerCase(),
   name: option.toLowerCase(),
-}));
+}))
 
-let tagsArray = [];
-let skillsArray = [];
+let tagsArray = []
+let skillsArray = []
 
 class FormCreateProject extends Component {
-  static contextType = UserContext;
+  static contextType = UserContext
 
   state = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
     tags: options,
     selected: [],
     skills: [],
-    category: "",
-    frequency: "",
-    status: "",
-    image: "",
-    description: "",
-    title: "",
-    tempUrl: "",
-  };
+    category: '',
+    frequency: '',
+    status: '',
+    image: '',
+    description: '',
+    title: '',
+    tempUrl: '../../media/avatar-project.png',
+  }
 
   handleChange = (event) => {
-    let key = event.target.name;
+    let key = event.target.name
     switch (event.target.type) {
-      case "file":
+      case 'file':
         this.setState({
           tempUrl: URL.createObjectURL(event.target.files[0]),
           image: event.target.files[0],
-        });
-        break;
-      case "checkbox":
-        this.setState({ [key]: event.target.checked });
-        break;
+        })
+        break
+      case 'checkbox':
+        this.setState({ [key]: event.target.checked })
+        break
       default:
-        this.setState({ [key]: event.target.value });
+        this.setState({ [key]: event.target.value })
     }
-  };
+  }
 
   handlePlace = (place) => {
-    this.setState({ location: place.place_name });
-  };
+    this.setState({ location: place.place_name })
+  }
 
   handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    let errors = { is: false, messages: [] };
+    let errors = { is: false, messages: [] }
 
     if (!this.state.title) {
-      errors.is = true;
-      errors.messages.push("You need a title for your project");
+      errors.is = true
+      errors.messages.push('You need a title for your project')
     }
     if (!this.state.description) {
-      errors.is = true;
-      errors.messages.push("You need a description for your project");
+      errors.is = true
+      errors.messages.push('You need a description for your project')
     }
 
     if (errors.is) {
-      this.setState({ errors: errors.messages });
+      this.setState({ errors: errors.messages })
     } else {
-      var formData = new FormData();
-      formData.append("image", this.state.image);
-      formData.append("title", this.state.title);
-      formData.append("description", this.state.description);
-      formData.append("skills", this.state.skills);
-      formData.append("location", this.state.location);
+      var formData = new FormData()
+      formData.append('image', this.state.image)
+      formData.append('title', this.state.title)
+      formData.append('description', this.state.description)
+      formData.append('skills', this.state.skills)
+      formData.append('location', this.state.location)
 
       this.state.category
-        ? formData.append("category", this.state.category)
-        : formData.append("category", "Covid-19");
+        ? formData.append('category', this.state.category)
+        : formData.append('category', 'Covid-19')
       this.state.frequency
-        ? formData.append("frequency", this.state.frequency)
-        : formData.append("frequency", "Regular");
+        ? formData.append('frequency', this.state.frequency)
+        : formData.append('frequency', 'Regular')
       this.state.status
-        ? formData.append("status", this.state.status)
-        : formData.append("status", "Open");
+        ? formData.append('status', this.state.status)
+        : formData.append('status', 'Open')
 
       apiHandler
         .createProject(formData)
         .then((data) => {
-          this.props.history.push("/user-profile");
+          this.props.history.push('/user-profile')
         })
         .catch((error) => {
-          errors.is = true;
-          errors.messages.push(error.response.data.message);
-          this.setState({ errors: errors.messages });
-        });
+          errors.is = true
+          errors.messages.push(error.response.data.message)
+          this.setState({ errors: errors.messages })
+        })
     }
-  };
+  }
 
   onSelect = (tag) => {
     const newTag = {
       label: tag.label,
       value: tag.value || tag.label,
       name: tag.name || tag.label,
-    };
-    tagsArray.push(newTag);
-    skillsArray.push(tag.name);
+    }
+    tagsArray.push(newTag)
+    skillsArray.push(tag.name)
 
     this.setState({
       selected: tagsArray,
       skills: skillsArray,
-    });
-  };
+    })
+  }
 
   remove = (tag) => {
     this.setState({
       selected: this.state.selected.filter((t) => t.value !== tag.value),
-    });
-  };
+    })
+  }
 
   render() {
-    let imgSrc = null;
+    let imgSrc = '../../media/avatar-project.png'
     if (!this.context.user) {
-      this.props.history.push("/");
-      return true;
-    } else if (this.state.tempUrl === "") {
-      imgSrc = null;
-    } else imgSrc = this.state.tempUrl;
+      this.props.history.push('/')
+      return true
+    } else if (this.state.tempUrl === '') {
+      imgSrc = null
+    } else imgSrc = this.state.tempUrl
     return (
       <div>
         {this.state.errors && (
           <ErrorMessageHandler messages={this.state.errors} />
         )}
         <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-          {this.state.image ? (
-            <div className='user__image round__image'>
-              <img src={imgSrc} alt='project' />
-            </div>
-          ) : null}
+          <div className="user__image round__image">
+            <img src={imgSrc} alt="project" />
+          </div>
 
-          <div className='form__group'>
-            <label htmlFor='image' className='custom-upload label'>
+          <div className="form__group">
+            <label htmlFor="image" className="custom-upload label">
               Upload project image
             </label>
-            <input className='input' type='file' id='image' name='image' />
+            <input className="input" type="file" id="image" name="image" />
           </div>
 
-          <div className='form__group'>
-            <label htmlFor='title'>Title</label>
+          <div className="form__group">
+            <label htmlFor="title">Title</label>
             <input
-              type='text'
-              id='title'
-              name='title'
-              placeholder='Enter project name'
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Enter project name"
             />
           </div>
 
-          <div className='form__group'>
-            <label htmlFor='description'>Description</label>
+          <div className="form__group">
+            <label htmlFor="description">Description</label>
             <textarea
-              type='textarea'
-              id='description'
-              name='description'
-              placeholder='Write something about this project'
+              type="textarea"
+              id="description"
+              name="description"
+              placeholder="Write something about this project"
             />
           </div>
-          <div className='form__group'>
-            <label htmlFor='category'>Category</label>
-            <select name='category'>
-              <option value='Covid-19'>Covid-19</option>
-              <option value='Education'>Education</option>
-              <option value='Arts'>Arts</option>
-              <option value='Animals'>Animals</option>
-              <option value='Environment'>Environment</option>
-              <option value='Poverty'>Poverty</option>
-              <option value='Equality'>Equality</option>
+          <div className="form__group">
+            <label htmlFor="category">Category</label>
+            <select name="category">
+              <option value="Covid-19">Covid-19</option>
+              <option value="Education">Education</option>
+              <option value="Arts">Arts</option>
+              <option value="Animals">Animals</option>
+              <option value="Environment">Environment</option>
+              <option value="Poverty">Poverty</option>
+              <option value="Equality">Equality</option>
             </select>
           </div>
-          <div className='form__group'>
-            <label htmlFor='skills'>Requested skills</label>
+          <div className="form__group">
+            <label htmlFor="skills">Requested skills</label>
             <TagBox
               tags={this.state.tags}
               selected={this.state.selected}
               onSelect={this.onSelect}
               removeTag={this.remove}
               backspaceDelete={true}
-              placeholder='What skills do you need?'
+              placeholder="What skills do you need?"
             />
-            <div className='checkbox'>
+            <div className="checkbox">
               <input
                 onChange={this.handleChange}
-                type='checkbox'
-                name='DontKnow'
+                type="checkbox"
+                name="DontKnow"
               ></input>
-              <label htmlFor='DontKnow'>
+              <label htmlFor="DontKnow">
                 I don't know what skills I need for my project
               </label>
             </div>
           </div>
-          <div className='form__group'>
-            <label htmlFor='location'>Location</label>
+          <div className="form__group">
+            <label htmlFor="location">Location</label>
             <LocationAutoComplete onSelect={this.handlePlace} />
           </div>
-          <div className='form__group'>
-            <label htmlFor='frequency'>Frequency</label>
-            <select name='frequency'>
-              <option value='Regular'>Regular</option>
-              <option value='Temporary'>Temporary</option>
+          <div className="form__group">
+            <label htmlFor="frequency">Frequency</label>
+            <select name="frequency">
+              <option value="Regular">Regular</option>
+              <option value="Temporary">Temporary</option>
             </select>
           </div>
           {/* <div className="form__group">
@@ -297,11 +295,11 @@ class FormCreateProject extends Component {
               <option value="Completed">Completed</option>
             </select>
           </div> */}
-          <Button type='primary'>Create project</Button>
+          <Button type="primary">Create project</Button>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default withRouter(FormCreateProject);
+export default withRouter(FormCreateProject)
